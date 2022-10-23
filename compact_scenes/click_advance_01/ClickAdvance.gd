fa6016bbc8dffc8cbf1d67d46a162004eba43730
Michael Reel <mykilr@gmail.com>
2022-10-23 19:37:36 +0100

    Click to render the next shader in the chain.

---
+++ compact_scenes/click_advance_01/ClickAdvance.gd
@@ -0,0 +1,115 @@
+extends Node2D
+
+
+const CLICK_SPEED : float = 0.5
+
+onready var display := $DisplayTextureRect
+onready var repeat_delay_timer := $RepeatDelayTimer
+
+onready var shader_path_config : Array = [
+	{"root": $HeightMap/NoiseGeneration, "capture": "noise"},
+	{"root": $HeightMap/EdgeAdjust, "capture": "edge", "texture" : "noise"},
+	{"root": $HeightMap/HeightAdjust, "capture": "height", "texture": "edge"},
+	{"root": $FlowMap, "capture": "flowmap", "texture": "height"},
+	{"root": $PointsMap, "capture": "points"},
+	{
+		"root": $FlowDrawer/FlowInput,
+		"capture": "flowdraw",
+		"texture": "points",
+		"shader_textures": {"flow_map": "flowmap"},
+		"feedback": 1,
+	},
+	{
+		"root": $FlowHeights, 
+		"capture": "flowheights", 
+		"shader_textures": {
+			"height_map": "height",
+			"flow_map": "flowdraw",
+			"point_map": "points",
+		},
+	},
+]
+
+var captures : Dictionary = {}
+
+var current_index : int = -1
+var current_root : Node2D
+var current_viewport : Viewport
+var current_texture_rect : TextureRect
+
+var click_wait := CLICK_SPEED
+
+
+func _ready():
+	_advance()
+
+func _advance():
+	# check we have more configurations to go
+	if current_index >= len(shader_path_config) - 1:
+		return
+	
+	# Index the next shader
+	current_index += 1
+	current_root = shader_path_config[current_index]["root"]
+	current_viewport = current_root.get_node("Viewport")
+	current_texture_rect = current_viewport.get_node("TextureRect")
+	
+	# Set a new texture, from captured images, if configured to do so
+	if "texture" in shader_path_config[current_index]:
+		var capture_name : String = shader_path_config[current_index]["texture"]
+		var texture = _get_captured_texture(capture_name)
+		current_texture_rect.texture = texture
+	
+	if "shader_textures" in shader_path_config[current_index]:
+		var shader_texures = shader_path_config[current_index]["shader_textures"]
+		for uniform_name in shader_texures:
+			var capture_name : String = shader_texures[uniform_name]
+			var texture = _get_captured_texture(capture_name)
+			current_texture_rect.material.set_shader_param(uniform_name, texture)
+	
+	# Switch display to show the current viewport
+	display.texture = current_viewport.get_texture()
+	
+	if "feedback" in shader_path_config[current_index]:
+		repeat_delay_timer.start()
+
+func _on_RepeatDelayTimer_timeout():
+	current_viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Wait until the frame has finished before getting the texture.
+	yield(VisualServer, "frame_post_draw")
+	
+	# Retrieve the captured image.
+	var repeat_img = current_viewport.get_texture().get_data()
+	var texture = ImageTexture.new()
+	texture.create_from_image(repeat_img)
+	current_texture_rect.texture = texture
+
+func _get_captured_texture(var capture_name : String) -> ImageTexture:
+	var capture_img : Image = captures[capture_name]
+	var texture = ImageTexture.new()
+	texture.create_from_image(capture_img)
+	return texture
+
+func _process(delta):
+	click_wait -= delta
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and click_wait <= 0.0:
+		print("capturing " + current_root.name)
+		_capture_viewport_image()
+		click_wait = CLICK_SPEED
+
+func _capture_viewport_image() -> void:
+	current_viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Wait until the frame has finished before getting the texture.
+	yield(VisualServer, "frame_post_draw")
+	
+	# Retrieve the captured image.
+	var img = current_viewport.get_texture().get_data()
+	var capture_name = shader_path_config[current_index]["capture"]
+	
+	# Store it incase we need it later
+	captures[capture_name] = img
+	
+	# Only advance after we've recorded the image,
+	# moving _advance call up to the calling _process causes it to be performed early
+	_advance()
+
